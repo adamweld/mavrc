fad66fc0895ccc65c284199e09a28d1030f610e5
Adam Weld <adam.weld@gmail.com>
2020-11-10 22:21:21 -0800

    updates

--- mavtest.py
+++ mavtest.py
@@ -8,11 +8,13 @@ from pymavlink import mavutil
 from argparse import ArgumentParser
 parser = ArgumentParser(description=__doc__)
 
-parser.add_argument("--baudrate", type=int,
+parser.add_argument("-b", "--baudrate", type=int,
                           help="master port baud rate", default=115200)
+'''
 parser.add_argument("--channel", type=string,
-                          help="RC channel to read, e.g. chan1_raw", default=default='chan8_raw')
-parser.add_argument("--device", required=True, help="serial device, e.g. /dev/ttyS3")
+                          help="RC channel to read, e.g. chan1_raw", default='chan8_raw')
+                          '''
+parser.add_argument("-d", "--device", required=True, help="serial device, e.g. /dev/ttyS3")
 parser.add_argument("--source-system", dest='SOURCE_SYSTEM', type=int,
                           default=255, help='MAVLink source system for this GCS')
 args = parser.parse_args()
@@ -25,12 +27,31 @@ def wait_heartbeat(m):
 
 
 # create a mavlink serial instance
-conn = mavutil.mavlink_connection(args.device, baud=args.baudrate, source_system=args.SOURCE_SYSTEM)
+try:
+    conn = mavutil.mavlink_connection(args.device, baud=args.baudrate, source_system=args.SOURCE_SYSTEM)
+except:
+    print('Connection Failed')
+    exit()
 
 # wait for the heartbeat msg to find the system ID
 wait_heartbeat(conn)
 
 while True:
+    try:
+        msg =  conn.recv_match(type = 'SYS_STATUS', blocking = True)
+        #print(msg.args.channel)
+        print('Load:    {}'.format(msg.load))
+        print('Battery: {}'.format(msg.voltage_battery)) 
+        time.sleep(.1)
+    except:
+        print('Connection Dropped')
+        break
+
+print('Closing')
+
+'''
+while True:
     msg =  conn.recv_match(type = 'RC_CHANNELS', blocking = True)
     #print(msg.args.channel)
     print(msg.chan1_raw)
+'''
